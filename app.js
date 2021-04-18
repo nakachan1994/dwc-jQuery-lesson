5da3b1d3d910ce1d70fbf2b3cd2cd91a42a20f00
yunakamura <nakachaaaan@gmail.com>
2021-04-18 11:04:05 +0900

    [start]5-6

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 $(function(){
-  $('.box1').slideDown(function(){
+  $('.box').slideDown(function(){
     $('.box1').css({
       'background-color':'#0000FF',
       'width':'200px',
       'height':'100px'
     }).slideUp();
+  });
+});
+
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color':'#0000FF'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color':'#FF0000'});
   });
 });
